0fa5d8afe803e79669b514663ecde38e19d22701
scott-wyatt <scottwyatt86@gmail.com>
2017-11-30 15:50:13 -0500

    [fix] ci

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('proxy-engine-ng App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo()
-    expect(page.getParagraphText()).toEqual('Welcome to app!')
+    expect(page.getParagraphText()).toEqual('Welcome to Proxy Engine with Angular!')
   })
 })
