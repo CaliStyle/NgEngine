3a29242b2820a53a9223c3215479c4c69bb4809a
scott-wyatt <scottwyatt86@gmail.com>
2018-01-15 12:05:09 -0500

    [fix] fix tests

--- src/app/ngEngine/ng-engine.spec.ts
+++ src/app/ngEngine/ng-engine.spec.ts
@@ -1,7 +1,15 @@
 import { NgEngine } from './'
 
+// Environment Stub from  angular cli
+import { environment } from '../../environments/environment'
+// App Config for NgEngine
+import * as appConfig from '../../appConfig'
+import * as fromRootReducers from '../root/store/reducers'
+import * as fromRootActions from '../root/store/actions'
+
+
 describe('NgEngine', () => {
   it('should create an NgEngine instance', () => {
-    expect(new NgEngine()).toBeTruthy()
+    expect(new NgEngine({environment, appConfig, fromRootReducers, fromRootActions})).toBeTruthy()
   })
 })
