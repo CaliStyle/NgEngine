43952d41eadee2ab63414226c7e04dc86a6abf51
scott-wyatt <scottwyatt86@gmail.com>
2018-03-25 09:04:33 -0400

    [fix] fix injection by using provider

--- src/app/ngEngine/ng-engine.interface.ts
+++ src/app/ngEngine/ng-engine.interface.ts
@@ -1,9 +1,9 @@
 export interface NgEngineConfiguration {
-  environment?: any,
-  appConfig?: any
+  environment?: {[key: string]: any},
+  appConfig?: {[key: string]: any}
 }
 
-export const NgEngineConfiguration: NgEngineConfiguration = {
+export const DefaultNgEngineConfiguration: NgEngineConfiguration = {
   environment: {
     development: true,
     staging: false,
