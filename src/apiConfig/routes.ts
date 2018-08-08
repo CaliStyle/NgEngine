868f345260487d5f25daaf934a5c59c044fe8b35
scott-wyatt <scottwyatt86@gmail.com>
2018-08-08 15:44:47 -0400

    [chore] upgrade

--- src/apiConfig/routes.ts
+++ src/apiConfig/routes.ts
@@ -7,66 +7,46 @@
  * @see http://fabrixjs.io/doc/config/routes.js
  */
 
-'use strict'
-
-export const routes = [
+export const routes = {
   // Handle 404s and more
-  {
-    method: [ 'GET' ],
-    path: '/404',
-    handler: 'ViewController.index',
+  '/404': {
+    'GET': 'ViewController.index',
     config: {}
   },
-  {
-    method: [ 'GET' ],
-    path: '/500',
-    handler: 'ViewController.index',
+  '/500': {
+    'GET': 'ViewController.index',
     config: {}
   },
-  {
-    method: [ 'GET' ],
-    path: '/403',
-    handler: 'ViewController.index',
+  '/403': {
+    'GET': 'ViewController.index',
     config: {}
   },
-  {
-    method: [ 'GET' ],
-    path: '/401',
-    handler: 'ViewController.index',
+  '/401': {
+    'GET': 'ViewController.index',
     config: {}
   },
   /**
    * Render the view
    */
   // Home
-  {
-    method: [ 'GET' ],
-    path: '/',
-    handler: 'ViewController.index',
+  '/': {
+    'GET': 'ViewController.index',
     config: {}
   },
 
   /**
    * Constrain the DefaultController.info handler to accept only GET requests.
    */
-  {
-    method: [ 'GET' ],
-    path: '/api/v1/default/info',
-    handler: 'DefaultController.info',
-    config: {
-      app: {
-        proxyRouter: {
-          ignore: true
-        }
-      }
-    }
+  '/api/v1/default/info': {
+    'GET': 'DefaultController.info',
+    config: {}
   }
 
   // Proxy Router
-  // {
+  // '/api/v1/proxy/*': {
   //   method: [ 'GET' ],
   //   path: '/api/v1/proxy/*',
   //   handler: 'ViewController.proxyRouter',
   //   config: {}
   // }
-]
+}
